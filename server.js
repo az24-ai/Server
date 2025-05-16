56c7c0e966f5704346d6937e69d0b4e5c25cf4ec
Ahmed <az24@fsu.edu>
2025-05-16 17:50:28 -0600

    Secure listening port

--- server.js
+++ server.js
@@ -54,8 +54,7 @@ wss.on('connection', (ws, request) => {
   });
 });
 
-// Start server
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
